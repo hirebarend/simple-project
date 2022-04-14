182b9caf88d34b5f4ba1be337b65c6fece87489c
Barend Erasmus <hirebarend@gmail.com>
2022-04-14 15:40:41 +0200

    update

--- SimpleProject.Infrastructure/Persistence/MongoDb/MongoDbDynamicRouteRepository.cs
+++ SimpleProject.Infrastructure/Persistence/MongoDb/MongoDbDynamicRouteRepository.cs
@@ -28,7 +28,7 @@ namespace SimpleProject.Infrastructure.Persistence.MongoDb
         {
             await _mongoCollection.InsertOneAsync(new
             {
-                Payload = BsonDocument.Create(dynamicRouteRequest.Payload),
+                // Payload = BsonDocument.Create(dynamicRouteRequest.Payload),
                 Reference = reference,
             });
         }
