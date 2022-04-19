61f9ba0162acd84c58a65db42fb88862ec88f7cc
Barend Erasmus <hirebarend@gmail.com>
2022-04-19 14:04:39 +0200

    update

--- Tests/main.js
+++ Tests/main.js
@@ -2,7 +2,7 @@ const axios = require("axios").default;
 const uuid = require("uuid");
 
 (async () => {
-  const durationInSeconds = 2;
+  const durationInSeconds = 30;
 
   const requestsPerSecond = 5;
 
@@ -10,12 +10,16 @@ const uuid = require("uuid");
 
   console.log(`numberOfRequests: ${numberOfRequests}`);
 
-  const interval = requestsPerSecond / 1000;
+  const interval = 1000 / requestsPerSecond;
 
   console.log(`interval: ${interval}`);
 
+  let sum = 0;
+
   for (let i = 0; i < numberOfRequests; i++) {
-    execute();
+    execute().then((x) => {
+      sum += x;
+    });
 
     await new Promise((resolve) => setTimeout(() => resolve(), interval));
   }
@@ -31,22 +35,27 @@ async function execute() {
 
     while (true) {
       const responseGet = await axios.get(
-        `https://function-app-5695.azurewebsites.net/api/Order/${reference}`
+        `https://function-app-5695.azurewebsites.net/api/Order/${responsePost.data.order.reference}`
       );
 
       if (
         responseGet.data &&
         (responseGet.data.state === 3 || responseGet.data.state === 4)
       ) {
-        return (
+        const duration =
           new Date(responseGet.data.updated).getTime() -
-          new Date(responseGet.data.created).getTime()
-        );
+          new Date(responseGet.data.created).getTime();
+
+        console.log(`duration: ${duration}`);
+
+        return duration;
       }
 
       await new Promise((resolve) => setTimeout(() => resolve(), 100));
     }
   } catch {
     console.log(`[ERROR]`);
+
+    return 0;
   }
 }
